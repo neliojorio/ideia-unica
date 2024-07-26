d98a6a71969f0e01dae9cdce88f7fc19654e1899
Nelio <neliojorio@gmail.com>
2024-07-26 07:32:09 -0400

    loader com style.module e tempo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Link from "next/link"
+import Loader from "./loader"
 
 function Home() {
     return (
         <div>
-            <h1>Home</h1>
             <Link href="/sobre">Acessar página Sobre</Link>
+            <Loader></Loader>
         </div>
     )
 }
---
+++ pages/loader.js
@@ -0,0 +1,16 @@
+import styles from "./styles.module.css"
+
+function Loader(){
+    return (
+        <div className={styles.wrapper}>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.circle}></div>
+        <div className={styles.shadow}></div>
+        <div className={styles.shadow}></div>
+        <div className={styles.shadow}></div>
+        </div>
+    )
+}
+
+export default Loader 

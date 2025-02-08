4f0165c8feff0a872dc023404d38be586e15ee85
Nélio Jório <nelio.jorio@gmail.com>
2025-02-07 23:05:59 -0400

    define project to simple digital clock

---
+++ README.md
@@ -0,0 +1,3 @@
+### notes
+[yt](https://www.youtube.com/watch?v=EW7m2WIvFgQ)
+[yt](https://www.youtube.com/watch?v=IyRUn0GocEc)
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,44 @@
-import Link from "next/link"
-import Loader from "./loader"
+import { useEffect, useState } from "react";
+import styles from './styles.module.css'
 
-function Home() {
-    return (
-        <div>
-            <Link href="/sobre">Acessar página Sobre</Link>
-            <Loader></Loader>
-        </div>
-    )
+export default function Home() {
+  const [horas, setHoras] = useState("00");
+  const [minutos, setMinutos] = useState("00");
+  const [segundos, setSegundos] = useState("00");
+
+  useEffect(() => {
+    const relogio = setInterval(() => {
+      const dateToday = new Date();
+      let hr = dateToday.getHours();
+      let min = dateToday.getMinutes();
+      let sec = dateToday.getSeconds();
+
+      if (hr < 10) hr = "0" + hr;
+      if (min < 10) min = "0" + min;
+      if (sec < 10) sec = "0" + sec;
+
+      setHoras(hr);
+      setMinutos(min);
+      setSegundos(sec);
+    }, 1000);
+
+    return () => clearInterval(relogio); // Cleanup do intervalo
+  }, []);
+
+  return (
+    <div className={styles.relogio}>
+      <div>
+        <span>{horas}</span>
+        <span className={styles.tempo}>Horas</span>
+      </div>
+      <div>
+        <span>{minutos}</span>
+        <span className={styles.tempo}>Minutos</span>
+      </div>
+      <div>
+        <span>{segundos}</span>
+        <span className={styles.tempo}>Segundos</span>
+      </div>
+    </div>
+  );
 }
-
-export default Home
